842ce464753bfce11511df4ff552fb554a1ad824
KosPavel <kos.pav007@yandex.ru>
2018-08-26 23:45:12 +0300

    added simple step-by-step animation

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ class Player():
         self.position_y = 400
         self.phi = 270               #angle in degrees for ship rotation
         self.DELTA_PHI = 5
-        self.size_const = 15         #const for ship size regulation
+        self.SIZE_CONST = 15         #const for ship size regulation
         self.alpha = 270             #angle for speed vector calculation
         self.DELTA_ALPHA = 5
         self.movement_speed = 0      #current speed value, pixels/tick
@@ -33,19 +33,20 @@ class Player():
         Below we are creating ship by drawing triangle. Each point calculated
         using angle phi for future simple realization of rotation.
         '''
-        ship = canvas.create_polygon(
-        self.position_x + self.size_const * math.cos(math.radians(self.phi)),
-        self.position_y + self.size_const * math.sin(math.radians(self.phi)),
-        self.position_x + self.size_const * math.cos(math.radians(self.phi)-60),
-        self.position_y + self.size_const * math.sin(math.radians(self.phi)-60),
-        self.position_x + self.size_const * math.cos(math.radians(self.phi)+60),
-        self.position_y + self.size_const * math.sin(math.radians(self.phi)+60),
-        outline = 'red', fill = 'green'
-                                    )
-        print(canvas.coords(ship))
+        self.shape = [
+        self.position_x + self.SIZE_CONST * math.cos(math.radians(self.phi)),
+        self.position_y + self.SIZE_CONST * math.sin(math.radians(self.phi)),
+        self.position_x + self.SIZE_CONST * math.cos(math.radians(self.phi)-60),
+        self.position_y + self.SIZE_CONST * math.sin(math.radians(self.phi)-60),
+        self.position_x + self.SIZE_CONST * math.cos(math.radians(self.phi)+60),
+        self.position_y + self.SIZE_CONST * math.sin(math.radians(self.phi)+60),
+                     ]
 
-    def moveForward(self, event): #need little correction, alpha flicks in case
-                                  #when it equals phi
+        self.ship = canvas.create_polygon(self.shape, outline = 'red', fill = 'green')
+
+    def moveForward(self, event):
+        if self.movement_speed < self.MOVEMENT_SPEED_MAX:
+            self.movement_speed += self.DELTA_MOVEMENT_SPEED
         '''
         Here below a problem with 0 deg = 360 deg resolved, by comparing deltas
         phi and alpha in two points: alpha will/won't cross zero.
@@ -53,8 +54,10 @@ class Player():
         if math.fabs(self.phi - self.alpha) < 180:
             if self.phi > self.alpha:
                 self.alpha += self.DELTA_ALPHA
-            else:
+            elif self.phi < self.alpha:
                 self.alpha -= self.DELTA_ALPHA
+            else:
+                pass
         elif math.fabs(self.phi - self.alpha) > 180:
             if (360 - self.phi) < self.phi:
                 if self.alpha >= 0:
@@ -77,11 +80,15 @@ class Player():
         print('position_y = ' + str(self.position_y))
 
     def moveBackward(self, event):
+        if self.movement_speed > self.MOVEMENT_SPEED_MIN:
+            self.movement_speed -= self.DELTA_MOVEMENT_SPEED
         if math.fabs(self.phi - self.alpha) < 180:
             if self.phi > self.alpha:
                 self.alpha += self.DELTA_ALPHA
-            else:
+            elif self.phi < self.alpha:
                 self.alpha -= self.DELTA_ALPHA
+            else:
+                pass
         elif math.fabs(self.phi - self.alpha) > 180:
             if (360 - self.phi) < self.phi:
                 if self.alpha >= 0:
@@ -116,22 +123,41 @@ class Player():
         print('Phi = ' + str(self.phi))
 
     def direction(self): #calculation of second point of speed vector
-        vect_x = self.movement_speed * math.cos(math.radians(self.phi -
-                 self.alpha)) + self.position_x
-        vect_y = self.movement_speed * math.sin(math.radians(self.phi -
-                 self.alpha)) + self.position_y
+        vect_x = self.movement_speed * math.cos(math.radians(self.alpha)) + self.position_x
+        vect_y = self.movement_speed * math.sin(math.radians(self.alpha)) + self.position_y
         self.speed_vector = [vect_x, vect_y]
         print('speed_vector = ' + str(self.speed_vector))
 
-# class Game():
-#
-#     def update():
+    def shape_coords_update(self): #new coords for ship, using in .after
+        updated_shape = [
+        player.position_x + player.SIZE_CONST * math.cos(math.radians(player.phi)),
+        player.position_y + player.SIZE_CONST * math.sin(math.radians(player.phi)),
+        player.position_x + player.SIZE_CONST * math.cos(math.radians(player.phi)-60),
+        player.position_y + player.SIZE_CONST * math.sin(math.radians(player.phi)-60),
+        player.position_x + player.SIZE_CONST * math.cos(math.radians(player.phi)+60),
+        player.position_y + player.SIZE_CONST * math.sin(math.radians(player.phi)+60),
+                        ]
+        for coord in updated_shape:
+            coord = math.ceil(coord)
 
+        return updated_shape
+
+class Game():
+
+    def __init__(self):
+        pass
+
+    def update(self):
+        # canvas.coords(player.ship, player.position_x, player.position_y)
+        # canvas.coords(player.ship, player.shape)
+        new_shape_coords = player.shape_coords_update()
+        canvas.coords(player.ship, new_shape_coords)
+        canvas.after(30, self.update)
 
 root = tkinter.Tk()
 canvas = tkinter.Canvas(root, height = 800, width = 800, bg = 'white')
 canvas.pack()
-# game = Game()
+game = Game()
 player = Player(root)
-# canvas.after(30, Game.update)
+canvas.after(30, game.update)
 root.mainloop()
