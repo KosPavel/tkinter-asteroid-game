979ec4319e5e41014b8b66d8708af8b7049c08d3
KosPavel <kos.pav007@yandex.ru>
2018-09-15 15:04:45 +0300

    asteroids has been added

--- settings.py
+++ settings.py
@@ -8,3 +8,12 @@ background = 'white'
 pos_x = width / 2
 pos_y = height / 2
 '''</player settings> '''
+
+''' <asteroids settings> '''
+min_size = 8
+max_size = 30
+min_speed = 5
+max_speed = 8
+max_asteroids = 30
+colors = ['black', 'red', 'green', 'blue', 'cyan', 'yellow', 'magenta']
+''' </asteroids settings> '''
